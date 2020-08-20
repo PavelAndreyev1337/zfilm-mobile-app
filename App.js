61da45c0a6a79e62f56abba3384295e6f300c371
PavelAndreyev <pavel.andreyev.98@gmail.com>
2020-08-20 20:11:07 +0300

    created component for search form

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container } from 'native-base';
+import SearchForm from './components/SearchForm'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <SearchForm/>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/SearchForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {StyleSheet} from "react-native"
+import {Input, Container,Content, Form, Item, Button,Icon} from 'native-base'
+
+const SearchForm = () => {
+    return (
+        <Container>
+            <Content>
+                <Form>
+                    <Item style={styles.marginItem}>
+                        <Input placeholder="Search..." />
+                    </Item>
+                    <Button style={styles.marginItem} block primary>
+                        <Icon name="search"></Icon>
+                    </Button>
+                </Form>
+            </Content>
+        </Container>
+    )
+}
+
+const styles = StyleSheet.create({
+    marginItem: {
+        margin:10
+    }
+})
+
+export default SearchForm
